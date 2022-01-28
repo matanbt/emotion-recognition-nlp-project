6f4da0814ba5694f97eed8499ce9fb156bab7c30
ShirFrenkel <65973610+ShirFrenkel@users.noreply.github.com>
2022-01-28 11:29:29 +0200

    Create evaluate.py
    Copied it from original evaluate function (no changes yet)

---
+++ src/evaluate.py
@@ -0,0 +1,90 @@
+import argparse
+import json
+import logging
+import os
+import glob
+
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from tqdm import tqdm, trange
+from attrdict import AttrDict
+
+from transformers import (
+    BertConfig,
+    BertTokenizer,
+    AdamW,
+    get_linear_schedule_with_warmup
+)
+
+import data_utils
+from model_baseline import BertForMultiLabelClassification
+from utils import (
+    init_logger,
+    init_tensorboard_writer,
+    set_seed,
+    compute_metrics_classification
+)
+from data_loader import (
+    GoEmotionsProcessor
+)
+
+from train import train
+
+def evaluate(args, model, eval_dataset, mode, logger, tb_writer, compute_metrics, global_step=None):
+    results = {}
+    eval_sampler = SequentialSampler(eval_dataset)
+    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.eval_batch_size)
+
+    # Eval!
+    if global_step != None:
+        logger.info("***** Running evaluation on {} dataset ({} step) *****".format(mode, global_step))
+    else:
+        logger.info("***** Running evaluation on {} dataset *****".format(mode))
+    logger.info("  Num examples = {}".format(len(eval_dataset)))
+    logger.info("  Eval Batch size = {}".format(args.eval_batch_size))
+    eval_loss = 0.0
+    nb_eval_steps = 0
+    preds = None
+    out_label_ids = None
+
+    for batch in tqdm(eval_dataloader, desc="Evaluating"):
+        model.eval()
+
+        with torch.no_grad():
+            outputs = model(**batch)
+            tmp_eval_loss, logits = outputs[:2]
+
+            eval_loss += tmp_eval_loss.mean().item()
+        nb_eval_steps += 1
+        if preds is None:
+            preds = 1 / (1 + np.exp(-logits.detach().cpu().numpy()))  # Sigmoid
+            out_label_ids = batch["one_hot_labels"].detach().cpu().numpy()
+        else:
+            preds = np.append(preds, 1 / (1 + np.exp(-logits.detach().cpu().numpy())), axis=0)  # Sigmoid
+            out_label_ids = np.append(out_label_ids, batch["one_hot_labels"].detach().cpu().numpy(), axis=0)
+
+    eval_loss = eval_loss / nb_eval_steps
+    results = {
+        "loss": eval_loss
+    }
+    preds[preds > args.threshold] = 1
+    preds[preds <= args.threshold] = 0
+    result = compute_metrics(out_label_ids, preds)
+    results.update(result)
+
+    output_dir = os.path.join(args.output_dir, mode)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    output_eval_file = os.path.join(output_dir, "{}-{}.txt".format(mode, global_step) if global_step else "{}.txt".format(mode))
+    with open(output_eval_file, "w") as f_w:
+        logger.info("***** Eval results on {} dataset *****".format(mode))
+        for key in sorted(results.keys()):
+            logger.info("  {} = {}".format(key, str(results[key])))
+            f_w.write("  {} = {}\n".format(key, str(results[key])))
+
+    # logs the results to TensorBoard
+    tb_writer.add_scalars(mode, results, global_step=global_step)
+
+    return results
