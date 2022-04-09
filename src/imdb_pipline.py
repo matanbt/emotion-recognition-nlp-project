6ef548a3fc84aabd97680ced535d6d8c1095f8be
MatanBT <mtnbt123@gmail.com>
2022-04-09 17:33:35 +0300

    Added zero-shot experiments

--- src/imdb_pipline.py
+++ src/imdb_pipline.py
@@ -7,13 +7,20 @@ import numpy as np
 
 from attrdict import AttrDict
 from catboost.datasets import imdb
+from sklearn import svm
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from torch.utils.data import SequentialSampler, DataLoader
 
 from transformers import (
     BertConfig, BertTokenizer,
 )
+
+from datasets import load_dataset
+import datasets
+from xgboost import XGBClassifier
 
 from src.models.model_regression import BertForMultiDimensionRegression
 from src.model_args import model_choices
@@ -101,10 +108,9 @@ def pipline_imdb():
 
     # load imb
     dataset_size = 5000 # how much to select from the dataset
-    from datasets import load_dataset
     dataset = load_dataset("imdb")
-    eval_imdb = dataset['test']  #.shuffle().select(list(range(dataset_size)))  # can shuffle and select
-    eval_imdb = data_processor.process_new_dataset(dataset_size, 200)  # TODO change max_len
+    eval_imdb = dataset['train'].shuffle()  #.select(list(range(dataset_size)))  # can shuffle and select
+    eval_imdb = data_processor.process_new_dataset(eval_imdb, 200)  # TODO change max_len
     eval_imdb.set_format(type='torch',
                          columns=['input_ids', 'attention_mask', 'token_type_ids', 'label'])
     dl_eval_imdb = torch.utils.data.DataLoader(eval_imdb, batch_size=1)
@@ -121,8 +127,9 @@ def pipline_imdb():
             # print(f">> VAD = {predicted_vads[i]} || [real] Label = {example['label']}")
 
     # Save IMDB vads (test)
-    np.savetxt(os.path.join(PATH_CACHED_MODEL_DIR, "imdb_test_predicted_vads.csv"), predicted_vads)
+    np.savetxt(os.path.join(PATH_CACHED_MODEL_DIR, "imdb_train_predicted_vads.csv"), predicted_vads)
 
+    # TODO logistic regression from VAD to SENTIMENT
     # Calculate some metrics
     thresholds = np.arange(0, 1, 0.05)
     accuracy_by_threshold = np.zeros(len(thresholds))
@@ -131,7 +138,103 @@ def pipline_imdb():
         print(f">> threshold = {threshold} || accuracy={acc}")
         accuracy_by_threshold[i] = acc
 
-    px.bar(thresholds, accuracy_by_threshold).show()
+    px.bar(thresholds, accuracy_by_threshold).show()  # 0.69 basic
+
+
+def logistic_reg_imdb():
+    pred_vads_train = np.loadtxt(os.path.join(PATH_CACHED_MODEL_DIR, "imdb_train_predicted_vads.csv"))
+    pred_vads_test = np.loadtxt(os.path.join(PATH_CACHED_MODEL_DIR, "imdb_test_predicted_vads.csv"))
+
+    dataset = load_dataset("imdb")
+
+    log_regr = LogisticRegression() # penalty ='elasticnet'
+    log_regr = log_regr.fit(pred_vads_train, dataset['train']['label'])
+
+    print(f">> Accuracy on training: {log_regr.score(pred_vads_train, dataset['train']['label'])}")
+    print(f">> Accuracy on test: {log_regr.score(pred_vads_test, dataset['test']['label'])}")
+
+
+def pipline_emoint():
+    model_dict = load_model()
+    model = model_dict['model']
+    data_processor = model_dict['data_processor']
+    emo_lbl_idx_to_vad = data_processor.get_emotions_vads_lst()
+
+    # Load DS
+    dataset = datasets.load_dataset('csv', data_files='data/emoint/emoint.tsv',
+                                    sep='\t', header=None)
+    dataset['train'] = dataset['train'].rename_columns({'0': "id", '1':"text", '2':'label', '3':'volume'})
+    eval_emoint = dataset['train']  # .shuffle()
+
+    # Preprocess helpers
+    label2id = {
+        'anger': 0,
+        'fear': 1,
+        'joy': 2,
+        'sadness': 3,
+    }
+    def _hf_label_mapper(example):
+        example['label'] = label2id[example['label']]
+        return example
+
+    # Preprocess dataset
+    eval_emoint = eval_emoint.map(_hf_label_mapper)
+    eval_emoint = data_processor.process_new_dataset(eval_emoint, 50)
+    eval_emoint.set_format(type='torch',
+                           columns=['input_ids', 'attention_mask', 'token_type_ids', 'label'])
+    dl_eval_emotint = torch.utils.data.DataLoader(eval_emoint, batch_size=1)
+
+    # Predict VAD with our model
+    predicted_vads = torch.zeros((len(eval_emoint), 3))
+
+    model.eval()
+    with torch.no_grad():
+        dl_eval_emotint = tqdm(dl_eval_emotint, desc="Evalutation on EmoInt")
+        for i, example in enumerate(dl_eval_emotint):
+            outputs = model(**example)
+            predicted_vads[i] = outputs[0]
+
+            # print(f">> VAD = {predicted_vads[i]} || [real] Label = {example['label']}")
+
+    # Save IMDB vads (test)
+    np.savetxt(os.path.join(PATH_CACHED_MODEL_DIR, "emoint_train_predicted_vads.csv"), predicted_vads)
+    np.savetxt(os.path.join(PATH_CACHED_MODEL_DIR, "emoint_train_labels.csv"), np.array(eval_emoint['label']))
+    print("")
+
+
+def svm_emoint():
+    id2label = {
+        'anger': 0,
+        'fear': 1,
+        'joy': 2,
+        'sadness': 3,
+    }
+    predicted_vads = np.loadtxt(os.path.join(PATH_CACHED_MODEL_DIR, "emoint_train_predicted_vads.csv"))
+    labels = np.loadtxt(os.path.join(PATH_CACHED_MODEL_DIR, "emoint_train_labels.csv"))
+    labels = np.expand_dims(labels, axis=-1).astype(int)
+    X_train, X_test, y_train, y_test = train_test_split(predicted_vads, labels, test_size=0.2)
+
+    model = svm.SVC()
+    model.fit(X_train, y_train)
+
+    print(f">> [SVM] Accuracy on training: {model.score(X_train, y_train)}")
+    print(f">> [SVM] Accuracy on test: {model.score(X_test, y_test)}")
+
+    model = XGBClassifier(num_class=4)
+    model.fit(X_train, y_train)
+
+    print(f">> [XGBoost] Accuracy on training: {model.score(X_train, y_train)}")
+    print(f">> [XGBoost] Accuracy on test: {model.score(X_test, y_test)}")
+
+def pipline_isaer():
+    dataset = datasets.load_dataset('csv', data_files='data/isear/isear.csv')
+
+    print("")
+
+def emotion_pipeline():
+    pass
+
 
 if __name__ == '__main__':
-    pipline_imdb()
+    pipline_emoint()
+    svm_emoint()
